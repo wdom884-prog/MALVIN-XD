d05b2fd6a264384726b4830d13eca141879f75c6
wdom884-prog <wdom884@gmail.com>
2025-08-24 16:00:25 -0700

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~ZuEwBSDS#RyWfy_nCnMwEkYCtqmZSXaDnKZAhxOZIXzNHrYiVdCk",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2348070982236", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
